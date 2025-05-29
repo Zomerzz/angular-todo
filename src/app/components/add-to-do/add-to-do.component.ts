cb2ae58b7ae4cbea7199ec2e83e144a12baef7d7
N9C9 <nicolo.casertano@gmail.com>
2025-05-29 19:23:59 +0200

    aggiorna metodi in "to-do-services.ts" e da implementare in "add-to-do-components.ts"  da riga 12

--- src/app/components/add-to-do/add-to-do.component.ts
+++ src/app/components/add-to-do/add-to-do.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
+import { ToDo } from '../../models/to-do';
+import { NgForm } from '@angular/forms';
+import { ToDoService } from '../../services/to-do-service';
 @Component({
   selector: 'app-add-to-do',
   imports: [RouterModule],
@@ -7,5 +10,16 @@ import { RouterLink, RouterModule } from '@angular/router';
   styleUrl: './add-to-do.component.css'
 })
 export class AddToDoComponent {
-
+  private _service :ToDoService = new ToDoService;
+  todo: Partial<ToDo> = {
+    title: "",
+    description: "",
+    deadline: "",
+    categoryId: 0
+   };
+  sendForm(f : NgForm) {
+    
+  }
+  
 }
+
--- src/app/services/to-do-service.ts
+++ src/app/services/to-do-service.ts
@@ -1,52 +1,38 @@
-import { Injectable } from "@angular/core";
+import { inject, Inject, Injectable } from "@angular/core";
 import { ToDo } from "../models/to-do";
+import { HttpClient } from "@angular/common/http";
+import { ToDoListComponent } from "../components/to-do-list/to-do-list.component";
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: "root"
 })
+
 export class ToDoService {
-    toDoList: ToDo[] = [
-        {
-            toDoId: 1,
-            title: "Aggiornare Git",
-            description: "finire i progetti",
-            deadline: undefined,
-            creationDate: new Date().toISOString().split('T')[0],
-            completedDate: undefined,
-            isCompleted: false,
-            categoryId: 1
-        },
-        {
-            toDoId: 2,
-            title: "Studiare",
-            description: "studiare matematica",
-            deadline: undefined,
-            creationDate: new Date().toISOString().split('T')[0],
-            completedDate: undefined,
-            isCompleted: false,
-            categoryId: 2
-        },
-        {
-            toDoId: 3,
-            title: "Finire il progetto",
-            description: "finire il to do list",
-            deadline: undefined,
-            creationDate: new Date().toISOString().split('T')[0],
-            completedDate: undefined,
-            isCompleted: false,
-            categoryId: 3
-        }
-    ]
+    private _url: string = "http://localhost:8080/api/todos";
+    private _http: HttpClient = inject(HttpClient);
 
-    getToDos():ToDo[]{
-        return this.toDoList;
+    findAll(): Observable<ToDo[]> {
+        return this._http.get<ToDo[]>(this._url);
     }
-    deleteToDo(obj: {toDoId: number, title: string}): boolean{
-        let lengthArr = this.toDoList.length;
-        this.toDoList=this.toDoList.filter(e => e.toDoId != obj.toDoId);
-        return lengthArr != this.toDoList.length;
 
+    deleteToDo(toDoId: number): Observable<Boolean>{
+        return this._http.delete<Boolean>(`${this._url}/${toDoId}`);
         // chiamata al backEnd pre fare la delete
     }
+
+    postToDo(toDo :Partial<ToDo>): Observable<ToDo> {
+        let creationDate : string = new Date().toISOString().split('T')[0];
+        toDo.creationDate = creationDate;
+        return this._http.post<ToDo>(this._url, toDo);
+    }
+
+    upDateToDo(toDo : ToDo): Observable<ToDo> {
+        return this._http.put<ToDo>(`${this._url}/${toDo.toDoId}`, toDo);
+    }
+
+    findById(toDoId :number): Observable<ToDo> {
+        return this._http.get<ToDo>(`${this._url}/${toDoId}`);
+    } 
 
 }
